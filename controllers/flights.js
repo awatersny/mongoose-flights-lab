606d9ee48391474822e87f2d992c519cd0b3cf06
awatersny <awatersny@gmail.com>
2022-02-27 13:49:46 -0500

    Set up route for flights list

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,9 +1,14 @@
 import { Flight } from "../models/flight.js";
 
+function index(req, res) {
+  res.render("flights/index");
+}
+
 function newFlight(req, res) {
   res.render("flights/new");
 }
 
 export {
+  index,
   newFlight as new
 }
