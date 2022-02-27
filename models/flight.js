d4a821cfb31c4fc9c5dc09bc2617c773a8ba5610
awatersny <awatersny@gmail.com>
2022-02-26 21:24:27 -0500

    Add validations

--- models/flight.js
+++ models/flight.js
@@ -3,8 +3,21 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-  airline: String,
-  airport: String,
-  flightno: Number,
-  departs: Date
+  airline: {
+    type: String,
+    enum: ["American", "Southwest", "United"]
+  },
+  airport: {
+    type: String,
+    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+    default: "DEN"
+  },
+  flightno: {
+    type: Number,
+    min: 10,
+    max: 9999
+  },
+  departs: {
+    type: Date
+  }
 });
