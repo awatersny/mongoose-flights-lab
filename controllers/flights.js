a2bfa51950f1ddd1b456fa67c8c4b201d77d4a1a
awatersny <awatersny@gmail.com>
2022-02-27 21:27:26 -0500

    Persist data in new flight form

--- controllers/flights.js
+++ controllers/flights.js
@@ -14,8 +14,19 @@ function newFlight(req, res) {
 }
 
 function create(req, res) {
+  // 2022-05-19T19:00
+  // 
+  req.body.departs = req.body.departs.replace('T', ' ');
+
   const flight = new Flight(req.body);
   console.log(flight);
+
+  flight.save(err => {
+    if(err){
+      return res.redirect('/flights/new');
+    }
+    res.redirect('/flights');
+  });
 }
 
 export {
