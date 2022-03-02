0415968933bb63ab38937210013868c31afbdd9d
awatersny <awatersny@gmail.com>
2022-03-01 19:33:20 -0500

    Test and debug router connection for new meals

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,2 +1,9 @@
 import { Meal } from "../models/meals.js"
 
+function newMeal(){
+  console.log(1);
+}
+
+export {
+  newMeal as new
+}
--- routes/meals.js
+++ routes/meals.js
@@ -1,7 +1,9 @@
 import { Router } from 'express'
+import * as mealsCtrl from '../controllers/meals.js'
+
 const router = Router()
 
-
+router.get("/new", mealsCtrl.new);
 
 export { 
   router
