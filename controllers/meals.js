db3c68a782aaa99c772f1dacd376c7088baa21d5
awatersny <awatersny@gmail.com>
2022-03-01 20:05:55 -0500

    Establish router connection to new meals

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,7 +1,9 @@
 import { Meal } from "../models/meals.js"
 
-function newMeal(){
-  console.log(1);
+function newMeal(req, res){
+  res.render("meals/new", {
+    title: "Add Meal"
+  });
 }
 
 export {
